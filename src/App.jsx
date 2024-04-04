9ba05cf31159555bfb0f7e2e8847c3442d089282
mohammadhossein <hossein.taeezadeh17@gmail.com>
2024-04-04 16:32:08 +0330

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  HashRouter, Route, Routes } from "react-router-dom";
 
 import { ThemIcon, myThem } from "./asset/them/themSetting";
 import {
@@ -19,7 +19,7 @@ export default function App() {
     <ThemIcon.Provider value={myThem}>
       <div className=" w-full h-screen p-[40px]" style={{background:checkThem? "radial-gradient(circle, rgba(214,221,225,1) 0%, rgba(227,226,226,1) 51%)":"radial-gradient(circle, rgba(65,65,65,1) 0%, rgba(46,46,46,1) 51%)" , overflow:'hidden'}} >
         <div className="w-full h-full border-t-solid border-[gray] border-l-2 border-b-2 rounded-bl-[10px]">
-          <BrowserRouter>
+          <HashRouter base="/">
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/Projects" element={<Projects />}></Route>
@@ -28,7 +28,7 @@ export default function App() {
               
               <Route path="/About" element={<About />}></Route>
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </div>
       </div>
     </ThemIcon.Provider>
